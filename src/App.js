9edff4c4c6a33942ca7ed1b3bd6570e1e0397468
Edgar Benzor <edgarbenz@hotmail.com>
2022-04-05 13:07:30 -0500

    Applying styles using Styled Components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 import TweetsPage from './components/tweets/TweetPage'
+import Button from './components/shared/Button'
 
 function App() {
   return (
     <div className="App">
+      <Button variant="primary">Log in</Button>
       <TweetsPage className="page" />
     </div>
   );
